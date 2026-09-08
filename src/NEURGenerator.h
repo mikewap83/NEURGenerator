f7a44bd5e1ff3043aedb2c798b61635dcaad5338
mikewap83 <113021062+mikewap83@users.noreply.github.com>
2026-09-08 09:41:08 +0700

    Update NEURGenerator.h

--- src/NEURGenerator.h
+++ src/NEURGenerator.h
@@ -52,11 +52,11 @@ constexpr size_t   sz_enc_prompt =  8192;
 constexpr size_t   sz_enc_denial =  2048;
 constexpr size_t   sz_JsonBuffer =  4096;
 
-constexpr size_t  sz_api_models  =    64;
-constexpr size_t  sz_api_pollen  =    32;
-constexpr size_t  sz_model_names =    64;
-constexpr size_t  sz_model_title =   128;
-constexpr size_t  sz_model_price =    32;
+constexpr size_t  sz_api_models  =   256;
+constexpr size_t  sz_api_pollen  =   128;
+constexpr size_t  sz_model_names =   256;
+constexpr size_t  sz_model_title =   512;
+constexpr size_t  sz_model_price =   128;
 
 constexpr size_t  BUF_EXPAND_INT =  65536;
 constexpr size_t  BUF_EXPAND_MAX = 786432;
